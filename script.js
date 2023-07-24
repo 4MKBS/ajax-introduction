97f76e74efd81953f9322221f35038856740466b
4MKBS <mkb4sakib@google.com>
2023-07-25 00:59:51 +0600

    fix

---
+++ script.js
@@ -0,0 +1,28 @@
+function loaddata(){
+    const xhr=new XMLHttpRequest();//create a new request
+
+    xhr.onload=function(){
+        const container=document.getElementById("demo");
+        demo.innerHTML=xhr.responseText;
+
+    };
+
+    xhr.open("GET","/data.txt");
+    xhr.send();
+    
+    setInterval(print,250);
+    
+}
+let i=1;
+function print(){
+    let div=document.getElementById('me');
+    
+        let a=document.createElement("p");
+        a.innerHTML=`<p style="color:black;">${i} sending data to server...</p>`;
+        div.appendChild(a);
+        i++;
+        window.scrollBy(1,50);
+        
+}
+
+loaddata();
